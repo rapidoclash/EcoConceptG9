4ba21adf343ad6bbc5465d86a382f1d0d2973255
Rodolphe <rodolphe.limousin@etu.unilim.fr>
2026-01-08 10:04:04 +0100

    Refacto product page

--- sujet/site/produits.php
+++ sujet/site/produits.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+	session_start();
 ?>
 <!DOCTYPE html>
 
@@ -8,29 +8,54 @@ session_start();
 	<title>TRUC</title>
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
-<!--*************** MENU ***************-->
-<?php
-	include "views/partials/_header.php";
-?>
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-<!--*************** END MENU ***************-->
+	<!--*************** MENU ***************-->
+	<?php
+		include "views/partials/_header.php";
+	?>
+	<!--*************** END MENU ***************-->
 	
+	<!--*************** LISTE PRODUITS ***************-->
 	<main id="container">
-	</main>
-	
-<!--*************** PIED DE PAGE ***************-->
-<?php
-	include "views/partials/_footer.php";
-?>
+		<?php
+			require("metier/DB_connector.php");
+			require("metier/Produit.php");
+			require("Dao/ProduitDao.php");
 
-<!--*************** PIED DE PAGE ***************-->
-	<script src="scripts/initListeProduits.js"></script>
+			// Ouverture de la connexion BDD
+			$cnx = new DB_connector();
+			$jeton = $cnx->openConnexion();
+
+
+			// Création du manager permettant les actions en BDD
+			$produitManager = new ProduitDao($jeton);
+
+			$produits = $produitManager->getList();
+
+
+
+			for ($i = 0; $i < count($produits); $i++) {
+				
+				$produit = "<ul class='main-list'>";
+				$produit .= "<li class='main-item'><p class='titre'>".$produits[$i]->getTitre()."</p></li>";
+				$produit .= "<li class ='main-item'><ul class ='sub-list'>";
+				$produit .= "<li class='sub-item'><p class='texte'>".$produits[$i]->getDescr()."</p></li>";
+				$produit .= "<li class='sub-item'><img class='image' src='images/".$produits[$i]->getImg()."'></li>";		
+				$produit .= "</ul></li></ul>";
+				echo $produit;
+			}
+		?>
+	</main>
+	<!--*************** END LISTE PRODUITS ***************-->
+	
+	<!--*************** PIED DE PAGE ***************-->
+	<?php
+		include "views/partials/_footer.php";
+	?>
+	<!--*************** END PIED DE PAGE ***************-->
 </body>
 
 </html>
